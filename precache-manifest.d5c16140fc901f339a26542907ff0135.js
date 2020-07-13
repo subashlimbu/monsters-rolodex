af37772c510d4060fbf13b2740095537ae894290
subashlimbu <prajan_l@hotmail.com>
2020-07-13 17:07:54 +0100

    Updates

---
+++ precache-manifest.d5c16140fc901f339a26542907ff0135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2bad05cc23c7c133c569e63d69b917",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "19145fd6825f452bf635",
+    "url": "/monsters-rolodex/static/css/main.5c4d4a59.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19145fd6825f452bf635",
+    "url": "/monsters-rolodex/static/js/main.c70da47e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
